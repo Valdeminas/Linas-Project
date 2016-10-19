e1b8ab792bdf9a4b2e31f3d571f11b1e9c7c6fd5
Gediminas Ziezys <gediminas-zi@cpma.lt>
2016-10-19 10:23:41 +0300

    adding files

---
+++ LinoMvc/Models/Design.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LinoMvc.Models
+{
+    public class Design
+    {
+        [StringLength(4)]
+        [RegularExpression(@"^[A-Z]{4}$")]
+        public string SKU { get; set; }
+    }
+}
